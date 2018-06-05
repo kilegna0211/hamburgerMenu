5d5f3bacd1f9e006f958526dd13f9c81e02214a6
Angelique Clery <angeliqueclery@yahoo.fr>
2018-06-05 23:42:18 +0200

    add three buttons

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+// button 1
 var button1 = document.querySelector('#button1');
-var hamburger = document.querySelector('.bar');
-console.log(hamburger)
+var hamburger1 = document.querySelector('#button1 .bar');
+
 button1.addEventListener('click', function() {
-      hamburger.classList.toggle('animate')
+      hamburger1.classList.toggle('animate')
+});
+
+// button2
+var button2 = document.querySelector('#button2');
+var hamburger2 = document.querySelector('#button2 .bar');
+
+button2.addEventListener('click', function() {
+      hamburger2.classList.toggle('animate')
+});
+
+// button3
+var button3 = document.querySelector('#button3');
+var hamburger3 = Array.from(document.querySelectorAll('#button3 .bar'));
+
+button3.addEventListener('click', function() {
+  hamburger3.forEach(bar => bar.classList.toggle('animate'));
+});
+
+// button 4
+var button4 = document.querySelector('#button4');
+var hamburger4 = Array.from(document.querySelectorAll('#button4 .bar'));
+
+button4.addEventListener('click', function() {
+  hamburger4.forEach(bar => bar.classList.toggle('animate'));
 });
